17b607ef556ed3935955fb4c055d01c04fc63e32
DemisoDaba <demo.nkmt1@gmail.com>
2023-09-19 23:45:43 -0700

    tests

---
+++ tests/test_console.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+"""Unittest module for the console"""
+
+import unittest
+import os
+import json
+import pycodestyle
+from io import StringIO
+from console import HBNBCommand
+from models.engine.file_storage import FileStorage
+from unittest.mock import patch
+from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
+
+
+class TestCommand(unittest.TestCase):
+    """Class that tests the console"""
+
+    def setUp(self):
+        """Function empties file.json"""
+        FileStorage._FileStorage__objects = {}
+        FileStorage().save()
+
+    @unittest.skipIf(os.getenv('HBNB_TYPE_STORAGE') == 'db', "Not FileStorage")
+    def test_create_fs(self):
+        """test the create command"""
+        pass
+
+    def testPycodeStyle(self):
+        """Pycodestyle test for console.py"""
+        style = pycodestyle.StyleGuide(quiet=True)
+        p = style.check_files(['console.py'])
+        self.assertEqual(p.total_errors, 0, "fix pep8")
+
+    def test_doc_console(self):
+        self.assertIsNotNone(HBNBCommand.__doc__)
+        self.assertIsNotNone(HBNBCommand.do_all.__doc__)
+        self.assertIsNotNone(HBNBCommand.do_create.__doc__)
+        self.assertIsNotNone(HBNBCommand.do_destroy.__doc__)
+        self.assertIsNotNone(HBNBCommand.do_quit.__doc__)
+        self.assertIsNotNone(HBNBCommand.do_EOF.__doc__)
+        self.assertIsNotNone(HBNBCommand.do_count.__doc__)
+        self.assertIsNotNone(HBNBCommand.do_update.__doc__)
+        self.assertIsNotNone(HBNBCommand.emptyline.__doc__)
+
+
+if __name__ == '__main__':
+    unittest.main()
