224a7a1036949937515e2fb23358adda636f2b14
DemisoDaba <demo.nkmt1@gmail.com>
2023-11-20 20:01:27 +0000

    Updated

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -4,7 +4,7 @@ from fabric.contrib import files
 from fabric.api import env, put, run
 import os
 
-env.hosts = ['100.25.191.25', '54.209.27.50']
+env.hosts = ['35.153.18.226', '100.26.244.69']
 
 
 def do_deploy(archive_path):
