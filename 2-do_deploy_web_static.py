d4bac640677bfe6e93e5d6471f70a90b2a06bdd0
Demiso Daba <125874545+DemisoDaba@users.noreply.github.com>
2023-11-21 18:06:43 +0300

    Update 2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -4,17 +4,12 @@ Fabric script based on the file 1-pack_web_static.py that distributes an
 archive to the web servers
 """
 
-from fabric import Connection, task
+from fabric.api import put, run, env
 from os.path import exists
+env.hosts = ['35.153.18.226', '100.26.244.69']
 
-env = {
-    "hosts": ['35.153.18.226', '100.26.244.69'],
-    "user": 'ubuntu',
-    "key_filename": '~/.ssh/school'
-}
 
-@task
-def do_deploy(c, archive_path):
+def do_deploy(archive_path):
     """distributes an archive to the web servers"""
     if exists(archive_path) is False:
         return False
@@ -22,15 +17,14 @@ def do_deploy(c, archive_path):
         file_n = archive_path.split("/")[-1]
         no_ext = file_n.split(".")[0]
         path = "/data/web_static/releases/"
-        c.put(archive_path, '/tmp/')
-        c.run('mkdir -p {}{}/'.format(path, no_ext))
-        c.run('tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, no_ext))
-        c.run('rm /tmp/{}'.format(file_n))
-        c.run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
-        c.run('rm -rf {}{}/web_static'.format(path, no_ext))
-        c.run('rm -rf /data/web_static/current')
-        c.run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
+        put(archive_path, '/tmp/')
+        run('mkdir -p {}{}/'.format(path, no_ext))
+        run('tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, no_ext))
+        run('rm /tmp/{}'.format(file_n))
+        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
+        run('rm -rf {}{}/web_static'.format(path, no_ext))
+        run('rm -rf /data/web_static/current')
+        run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
         return True
-    except Exception as e:
-        print(f"Error: {e}")
+    except:
         return False
