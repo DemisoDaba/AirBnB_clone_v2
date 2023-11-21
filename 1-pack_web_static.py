34e58326fbc743da99fbd279e9a328be825803f7
Demiso Daba <125874545+DemisoDaba@users.noreply.github.com>
2023-11-21 16:21:16 +0300

    Update 1-pack_web_static.py

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,18 +1,23 @@
 #!/usr/bin/python3
-"""generates a .tgz archive from the contents of the web_static folder
 """
+Fabric script to genereate tgz archive
+execute: fab -f 1-pack_web_static.py do_pack
+"""
+
 from datetime import datetime
-from fabric.api import local
+from fabric.api import *
 
 
 def do_pack():
-    """generate
     """
-    try:
-        local("mkdir -p versions")
-        res = local("tar -cvzf versions/web_static_{}.tgz web_static"
-                    .format(datetime.now().strftime("%Y%m%d%H%M%S")),
-                    capture=True)
-        return res
-    except Exception:
+    making an archive on web_static folder
+    """
+
+    time = datetime.now()
+    archive = 'web_static_' + time.strftime("%Y%m%d%H%M%S") + '.' + 'tgz'
+    local('mkdir -p versions')
+    create = local('tar -cvzf versions/{} web_static'.format(archive))
+    if create is not None:
+        return archive
+    else:
         return None
