23ba961e15ef24c7fd52242cf4c5993d7ed8e36f
Demiso Daba <125874545+DemisoDaba@users.noreply.github.com>
2023-11-22 09:01:36 +0300

    Update 7-states_list.py

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,31 +1,30 @@
-
 #!/usr/bin/python3
-"""starts a Flask web application
 """
-from flask import Flask, render_template
+Module Docs
+"""
+from flask import Flask
+from models import *
 from models import storage
-from models.state import State
-
 
 app = Flask(__name__)
-app.url_map.strict_slashes = False
+
+
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    """
+    Fucntion Docs
+    """
+    states = sorted(list(storage.all("State").values()), key=lambda x: x.name)
+    return render_template('7-states_list.html', states=states)
 
 
 @app.teardown_appcontext
-def close_db(exc):
-    """closes
+def teardown_db(exception):
+    """
+    Function Docs
     """
     storage.close()
 
 
-@app.route('/states_list')
-def states_list():
-    """display a HTML page
-    """
-    states = storage.all(State).values()
-    return render_template("7-states_list.html", states=states)
-
-
 if __name__ == '__main__':
-    app.run(debug=True)
-    app.run(host='0.0.0.0', port=5000)
+    app.run(port='5000', host='0.0.0.0')
