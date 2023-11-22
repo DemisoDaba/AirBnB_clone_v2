614d967b17161e6d87015c3bd7f6eb080dac04f6
Demiso Daba <125874545+DemisoDaba@users.noreply.github.com>
2023-11-22 08:53:50 +0300

    Update 8-cities_by_states.py

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,31 +1,28 @@
-
 #!/usr/bin/python3
-"""starts a Flask web application
 """
-from flask import Flask, render_template
+Module Docs
+"""
+from flask import Flask
+from models import *
 from models import storage
-from models.state import State
-
 
 app = Flask(__name__)
-app.url_map.strict_slashes = False
+
+
+@app.route('/cities_by_states', strict_slashes=False)
+def cities_by_states():
+    """display the states and cities listed in alphabetical order"""
+    states = storage.all("State").values()
+    return render_template('8-cities_by_states.html', states=states)
 
 
 @app.teardown_appcontext
-def close_db(exc):
-    """closes
+def teardown_db(exception):
+    """
+    Function Docs
     """
     storage.close()
 
 
-@app.route('/cities_by_states')
-def cities_by_states():
-    """display a HTML page
-    """
-    states = storage.all(State).values()
-    return render_template("8-cities_by_states.html", states=states)
-
-
 if __name__ == '__main__':
-    app.run(debug=True)
-    app.run(host='0.0.0.0', port=5000)
+    app.run(port='5000', host='0.0.0.0')
