d399f554a478832b4e77a4c17868aedb6eac4a0d
Demiso Daba <125874545+DemisoDaba@users.noreply.github.com>
2023-11-22 08:38:36 +0300

    Update 4-number_route.py

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -1,50 +1,41 @@
-
 #!/usr/bin/python3
-"""starts a Flask web application
 """
+starts a Flask web application
+"""
+
 from flask import Flask
-
-
 app = Flask(__name__)
-app.url_map.strict_slashes = False
 
 
-@app.route('/')
-def hello():
-    """prints "Hello HBNB!"
-    """
+@app.route('/', strict_slashes=False)
+def index():
+    """returns Hello HBNB!"""
     return 'Hello HBNB!'
 
 
-@app.route('/hbnb')
+@app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """prints “HBNB”
-    """
+    """returns HBNB"""
     return 'HBNB'
 
 
-@app.route('/c/<text>')
-def c_is_fun(text):
-    """prints "C" followed by the value of the text variable
-    """
-    return 'C {}'.format(text.replace('_', ' '))
+@app.route('/c/<text>', strict_slashes=False)
+def cisfun(text):
+    """display “C ” followed by the value of the text variable"""
+    return 'C ' + text.replace('_', ' ')
 
 
-@app.route('/python')
-@app.route('/python/<text>')
-def python_is_magic(text='is cool'):
-    """prints “Python ”, followed by the value of the text
-    """
-    return 'Python {}'.format(text.replace('_', ' '))
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def pythoniscool(text='is cool'):
+    """display “Python ”, followed by the value of the text variable"""
+    return 'Python ' + text.replace('_', ' ')
 
 
-@app.route('/number/<int:n>')
-def number(n):
-    """prints "n is a number” only if n is an integer
-    """
-    return '{:d} is a number'.format(n)
-
+@app.route('/number/<int:n>', strict_slashes=False)
+def imanumber(n):
+    """display “n is a number” only if n is an integer"""
+    return "{:d} is a number".format(n)
 
 if __name__ == '__main__':
-    app.run(debug=True)
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port='5000')
